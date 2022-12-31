0838d7fe2618ed5ac62447ea62cfb8be60af0121
Rasujon3 <rasujon3@gmail.com>
2022-12-31 23:45:04 +0600

    contact component  done

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,11 +1,82 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Contacts from "../componets/Contacts";
+
+import { allUsersRoute } from "../utils/APIRoutes";
 
 const Chat = () => {
+  const [contacts, setContacts] = useState([]);
+  const [currentUser, setCurrentUser] = useState(undefined);
+  const [currentChat, setCurrentChat] = useState(undefined);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    async function handleUser() {
+      if (!localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
+        navigate("/login");
+      } else {
+        setCurrentUser(
+          await JSON.parse(
+            localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)
+          )
+        );
+      }
+    }
+    handleUser();
+  }, [navigate]);
+
+  useEffect(() => {
+    async function handleUser() {
+      if (currentUser) {
+        if (currentUser.isAvatarImageSet) {
+          const data = await axios.get(`${allUsersRoute}/${currentUser._id}`);
+          setContacts(data.data);
+        } else {
+          navigate("/setAvatar");
+        }
+      }
+    }
+    handleUser();
+  }, [currentUser, navigate]);
+
+  const handleCurrentChat = (chat) => {
+    setCurrentChat(chat);
+  };
+
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
+    <Container>
+      <div className="container">
+        <Contacts
+          contacts={contacts}
+          currentUser={currentUser}
+          changeChat={handleCurrentChat}
+        />
+      </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+  align-items: center;
+  background-color: #131324;
+  .container {
+    height: 85vh;
+    width: 85vw;
+    background-color: #00000076;
+    display: grid;
+    grid-template-columns: 25% 75%;
+    @media screen and (min-width: 720px) and (max-width: 1080px) {
+      grid-template-columns: 35% 65%;
+    }
+  }
+`;
 
 export default Chat;
--- src/pages/SetAvatar.jsx
+++ src/pages/SetAvatar.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Buffer } from "buffer";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
